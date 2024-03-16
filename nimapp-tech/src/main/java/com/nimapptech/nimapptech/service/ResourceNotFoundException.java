a7a8787988eb6e0594c0069120a923e4e7569f0f
Saurabh Chaugule <saurabhchaugule888@gmail.com>
2024-03-16 19:20:40 +0530

    Nilmap_Comapany_Project_Task

---
+++ nimapp-tech/src/main/java/com/nimapptech/nimapptech/service/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.nimapptech.nimapptech.service;
+
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	
+	public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
